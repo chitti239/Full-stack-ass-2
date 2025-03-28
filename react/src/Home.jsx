f71ae1dbe1884cafef467b27fa8702f566ee8079
chitti239 <sharinis239@gmail.com>
2025-03-28 13:31:44 +0530

    completed

---
+++ react/src/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to my page</h1>
+      <p><strong>Name :</strong> Samsani Sharini</p>
+      <p>samsani.sharini.s89@kalvium.community</p>
+    </div>
+  )
+}
+
+export default Home
